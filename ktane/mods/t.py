a6c56575627645ca98dfb9bd19551290a8eaf7b2
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-04-22 20:42:04 -0400

    implement, 1st half

--- ktane/mods/t.py
+++ ktane/mods/t.py
@@ -3,6 +3,7 @@
 from typing import Final
 
 from ktane.directors import ModuleSolver
+from ktane.ask import talk
 
 #todo: turn the keys
 class TurnTheKeys(ModuleSolver):
@@ -10,5 +11,19 @@ class TurnTheKeys(ModuleSolver):
     name: Final = "Turn The Keys"
     required_edgework: Final = ()
 
+    right_keys_turned: bool
+
+    def custom_data_init(self) -> None:
+        self.right_keys_turned: bool = False
+
+    def on_this_solved(self) -> None:
+        self.right_keys_turned = True
+        super().on_this_solved()
+
     def stage(self) -> None:
-        pass
+        talk("This module and others like it have a number"
+             " displaying each module's priority.")
+        if not self.right_keys_turned:
+            talk("Turn each right key on modules of this type,"
+                 " in descending order of priority.")
+        talk("Turn the lowest priority left key that hasn't already been turned.")
