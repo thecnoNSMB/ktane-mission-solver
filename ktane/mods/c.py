3af57a1d3a5102a9fadbae42f4bfaee310eb2880
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-04-23 11:58:42 -0400

    stage part 1

--- ktane/mods/c.py
+++ ktane/mods/c.py
@@ -3,6 +3,8 @@
 from typing import Final
 
 from ktane.directors import ModuleSolver
+from ktane.ask import talk
+from ktane import ask
 
 __all__ = [
     "ColourFlash",
@@ -13,5 +15,8 @@ class ColourFlash(ModuleSolver):
     name: Final = "Colour Flash"
     required_edgework: Final = ()
 
+    valid_colors: Final = {"red", "yellow", "green", "blue", "magenta", "white"}
+
     def stage(self) -> None:
-        pass
+        talk("What color is the last word in the sequence?")
+        last_color = ask.str_from_set(self.valid_colors, print_options=True)
