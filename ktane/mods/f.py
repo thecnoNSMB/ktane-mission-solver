dd048eb1a34c7bda98293ed632cbfb6ada232bf9
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-05-26 04:18:06 -0400

    get input (first steps)

--- ktane/mods/f.py
+++ ktane/mods/f.py
@@ -2,7 +2,9 @@
 
 from typing import Final
 
-from ktane.directors import ModuleSolver, EdgeFlag
+from ktane.directors import ModuleSolver, EdgeFlag, Port
+from ktane.ask import talk
+from ktane import ask
 
 __all__ = [
     "FollowTheLeader",
@@ -15,5 +17,14 @@ class FollowTheLeader(ModuleSolver):
     required_edgework: Final = (EdgeFlag.PORTS, EdgeFlag.SERIAL, EdgeFlag.BATTERIES,
                                 EdgeFlag.INDICATORS)
 
+    valid_colors: Final = {'red', 'yellow', 'green', 'blue', 'black', 'white'}
+
     def stage(self) -> None:
-        pass
+        talk("What plugs are the wires connected to?")
+        wire_plugs = ask.list_from_set({'1', '2', '3', '4', '5', '6',
+                                        '7', '8', '9', '10', '11', '12'})
+        talk("The wires are connected in clockwise order.")
+        talk("Starting from plug 1, what colors are the wires in order?")
+        wires = ask.list_from_set(self.valid_colors, expected_len=len(wire_plugs))
+        if self.bomb.has_port(Port.RJ45) and ('4' in wire_plugs and '5' in wire_plugs):
+            pass
