ed3ad8ce3ddc3a2b04bba0ade40055bf8791fa66
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-05-28 16:58:26 -0400

    determine starting wire

--- ktane/mods/f.py
+++ ktane/mods/f.py
@@ -20,12 +20,21 @@ class FollowTheLeader(ModuleSolver):
     valid_colors: Final = {'red', 'yellow', 'green', 'blue', 'black', 'white'}
 
     def stage(self) -> None:
-        talk("What plugs are the wires connected to?")
+        talk("What plugs are the wires connected to, in clockwise order?")
         wire_plugs = ask.list_from_set({'1', '2', '3', '4', '5', '6',
                                         '7', '8', '9', '10', '11', '12'})
-        talk("The wires are connected in clockwise order.")
         talk("Starting from plug 1, what colors are the wires in order?")
         wire_colors = ask.list_from_set(self.valid_colors, expected_len=len(wire_plugs))
         wires = list(zip(wire_plugs, wire_colors))
+        current_index: int
         if self.bomb.has_port(Port.RJ45) and ('4' in wire_plugs and '5' in wire_plugs):
-            pass
+            current_index = wire_plugs.index('4')
+        elif str(self.bomb.batteries) in wire_plugs:
+            current_index = wire_plugs.index(str(self.bomb.batteries))
+        elif self.bomb.serial_first_digit in wire_plugs:
+            current_index = wire_plugs.index(self.bomb.serial_first_digit)
+        elif not self.bomb.has_indicator('CLR', True):
+            current_index = 0
+        else:
+            talk("Cut all of the wires in reverse order.")
+            return
