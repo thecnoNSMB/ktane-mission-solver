2275543b468591c5a233e38b06ca17602185120c
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-05-27 21:47:29 -0400

    data structuring

--- ktane/mods/f.py
+++ ktane/mods/f.py
@@ -25,6 +25,7 @@ class FollowTheLeader(ModuleSolver):
                                         '7', '8', '9', '10', '11', '12'})
         talk("The wires are connected in clockwise order.")
         talk("Starting from plug 1, what colors are the wires in order?")
-        wires = ask.list_from_set(self.valid_colors, expected_len=len(wire_plugs))
+        wire_colors = ask.list_from_set(self.valid_colors, expected_len=len(wire_plugs))
+        wires = list(zip(wire_plugs, wire_colors))
         if self.bomb.has_port(Port.RJ45) and ('4' in wire_plugs and '5' in wire_plugs):
             pass
