2745c0fab006f28b647574c16ad561edc3e4f85f
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-05-29 11:28:04 -0400

    set up loop

--- ktane/mods/f.py
+++ ktane/mods/f.py
@@ -26,15 +26,20 @@ class FollowTheLeader(ModuleSolver):
         talk("Starting from plug 1, what colors are the wires in clockwise order?")
         wire_colors = ask.list_from_set(self.valid_colors, expected_len=len(wire_plugs))
         wires = list(zip(wire_plugs, wire_colors))
-        current_index: int
+        start_index: int
         if self.bomb.has_port(Port.RJ45) and ('4' in wire_plugs and '5' in wire_plugs):
-            current_index = wire_plugs.index('4')
+            start_index = wire_plugs.index('4')
         elif str(self.bomb.batteries) in wire_plugs:
-            current_index = wire_plugs.index(str(self.bomb.batteries))
+            start_index = wire_plugs.index(str(self.bomb.batteries))
         elif self.bomb.serial_first_digit in wire_plugs:
-            current_index = wire_plugs.index(self.bomb.serial_first_digit)
+            start_index = wire_plugs.index(self.bomb.serial_first_digit)
         elif not self.bomb.has_indicator('CLR', True):
-            current_index = 0
+            start_index = 0
         else:
             talk("Cut all of the wires in reverse order.")
             return
+        current_index = start_index
+        talk(f"Cut the wire starting at plug {current_index}.")
+        current_index = (current_index + 1) % len(wire_plugs)
+        while current_index != start_index:
+            current_index = (current_index + 1) % len(wire_plugs)
