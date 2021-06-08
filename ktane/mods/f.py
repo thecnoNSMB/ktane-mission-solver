851c36a33c0a64c05a1896ca74a4dd8412c5789a
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-06-07 21:50:14 -0400

    develop Follow The Leader solver further, and split for readability

--- ktane/mods/f.py
+++ ktane/mods/f.py
@@ -1,6 +1,6 @@
 "Modded modules whose names begin with the letter F."
 
-from typing import Final
+from typing import Final, List
 
 from ktane.directors import ModuleSolver, EdgeFlag, Port
 from ktane.ask import talk
@@ -25,7 +25,6 @@ class FollowTheLeader(ModuleSolver):
                                         '7', '8', '9', '10', '11', '12'})
         talk("Starting from plug 1, what colors are the wires in clockwise order?")
         wire_colors = ask.list_from_set(self.valid_colors, expected_len=len(wire_plugs))
-        wires = list(zip(wire_plugs, wire_colors))
         start_index: int
         if self.bomb.has_port(Port.RJ45) and ('4' in wire_plugs and '5' in wire_plugs):
             start_index = wire_plugs.index('4')
@@ -38,8 +37,47 @@ class FollowTheLeader(ModuleSolver):
         else:
             talk("Cut all of the wires in reverse order.")
             return
+        self.wire_loop(wire_plugs, wire_colors, start_index)
+
+    def wire_loop(self, wire_plugs: List[str],  #pylint: disable=too-many-branches
+                  wire_colors: List[str], start_index: int) -> None:
+        "Given a starting index, determine which wires in the loop to cut."
+        rules_reverse_order = wire_colors[start_index] in ('red', 'green', 'white')
         current_index = start_index
         talk(f"Cut the wire starting at plug {current_index}.")
         current_index = (current_index + 1) % len(wire_plugs)
+        current_rule: int
+        if self.bomb.serial_first_letter:
+            current_rule = "abcdefghijklmnopqrstuvwxyz".index(
+                self.bomb.serial_first_letter) % 13
+        else:
+            current_rule = 0
         while current_index != start_index:
+            if current_rule == 0 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 1 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 2 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 3 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 4 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 5 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 6 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 7 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 8 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 9 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 10 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 11 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
+            elif current_rule == 12 and True:
+                talk(f"Cut the wire starting at plug {current_index}.")
             current_index = (current_index + 1) % len(wire_plugs)
+            current_rule = (current_rule + (-1 if rules_reverse_order else 1)) % 13
