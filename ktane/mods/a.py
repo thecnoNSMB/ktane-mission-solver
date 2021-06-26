0b36424bab39322ff89452e6dd2286150307dd87
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-06-26 02:26:25 -0400

    implement Anagrams

--- ktane/mods/a.py
+++ ktane/mods/a.py
@@ -15,3 +15,14 @@ class Anagrams(ModuleSolver):
     name: Final = "Anagrams"
     id: Final = "AnagramsModule"
     required_edgework: Final = ()
+
+    words: Final = {"stream", "master", "tamers", "looped", "poodle", "pooled",
+                    "cellar", "caller", "recall", "seated", "sedate", "teased",
+                    "rescue", "secure", "recuse", "rashes", "shears", "shares",
+                    "barely", "barley", "bleary", "duster", "rusted", "rudest"}
+
+    def stage(self) -> None:
+        talk("What word is on the display?")
+        word = ask.str_from_set(self.words)
+        anagrams = [w for w in self.words if set(w) == set(word) and w != word]
+        talk(f'Type in the word "{anagrams[0]}"')
