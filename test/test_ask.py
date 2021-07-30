beac2d6d305840a22c944030d310a11998891ca7
Gibus Wearing Mann <7275663+thecnoNSMB@users.noreply.github.com>
2021-07-29 23:46:32 -0400

    i can clean this up a bit

--- test/test_ask.py
+++ test/test_ask.py
@@ -6,7 +6,7 @@ from unittest.mock import patch
 from hypothesis import given, assume, strategies as st
 
 sys.path.append('..')
-import ktane.ask #pylint: disable=wrong-import-position #directory hack
+from ktane import ask #pylint: disable=wrong-import-position #directory hack
 
 @given(st.sets(st.text()), st.booleans(), st.booleans(), st.data())
 def test_str_from_set(inputs_set: Set[str], case_sensitive: bool, print_options: bool,
@@ -14,9 +14,8 @@ def test_str_from_set(inputs_set: Set[str], case_sensitive: bool, print_options:
     "Test that ask.str_from_set returns strings in the set passed to it."
     try:
         with patch("builtins.input", lambda _: data.draw(st.text())):
-            return_string = ktane.ask.str_from_set(inputs_set,
-                                                   case_sensitive=case_sensitive,
-                                                   print_options=print_options)
+            return_string = ask.str_from_set(inputs_set, case_sensitive=case_sensitive,
+                                             print_options=print_options)
             assert return_string in inputs_set
     except RuntimeError as message:
         #expected failure if too many inputs fail
@@ -32,10 +31,9 @@ def test_list_from_set(inputs_set: Set[str], case_sensitive: bool, print_options
     containing only strings in the set passed to it."""
     try:
         with patch("builtins.input", lambda _: data.draw(st.text())):
-            return_list = ktane.ask.list_from_set(inputs_set,
-                                                  case_sensitive=case_sensitive,
-                                                  print_options=print_options,
-                                                  expected_len=expected_len)
+            return_list = ask.list_from_set(inputs_set, case_sensitive=case_sensitive,
+                                            print_options=print_options,
+                                            expected_len=expected_len)
             assert all(s in inputs_set for s in return_list)
     except RuntimeError as message:
         #expected failure if too many inputs fail
@@ -49,7 +47,7 @@ def test_positive_int(data: st.DataObject) -> None:
     "Test that ask.positive_int actually returns positive nonzero integers."
     try:
         with patch("builtins.input", lambda _: data.draw(st.sampled_from('0123456789'))):
-            return_int = ktane.ask.positive_int()
+            return_int = ask.positive_int()
             assert return_int > 0
     except RuntimeError as message:
         #expected failure if too many inputs fail
@@ -59,7 +57,7 @@ def test_positive_int(data: st.DataObject) -> None:
             raise
 
 if __name__ == "__main__":
-    ktane.ask.ENABLE_PRINTING = False #type: ignore[misc] #disable printing for the test
+    ask.ENABLE_PRINTING = False #type: ignore[misc] #disable printing for the test
     test_str_from_set()
     test_list_from_set()
     test_positive_int()
